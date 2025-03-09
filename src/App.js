b005f9a715b43acb2eb6a88922faa908f4d763be
Tanyakalra20 <tanyakalra61@gmail.com>
2025-03-09 16:02:17 +0530

    Configured project for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import Dashboard from "./pages/Dashboard"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const[isLoggedIn, setIsLoggedIn] = useState(false);
-  return(
-    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col ">
-      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn ={setIsLoggedIn}/>
+  return (
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col">
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
-        <Route path = "/" element ={<Home/>}/>
-        <Route path="/login" element ={<Login setIsLoggedIn ={setIsLoggedIn} />} />
-        <Route path="/signup" element ={<Signup setIsLoggedIn ={setIsLoggedIn}/>} />
-        <Route path="/dashboard" element ={
-          <PrivateRoute isLoggedIn={isLoggedIn}>
-            <Dashboard/>
-          </PrivateRoute>
-          } />
-
-
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute isLoggedIn={isLoggedIn}>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
